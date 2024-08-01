375af5d08f09669664ed6851ae85b8fd098ac8d9
matuzalembatista <matuzalem_pereira@hotmail.com>
2024-08-01 19:48:05 -0300

    first commit

---
+++ src/main/java/com/example/projeto_sistema_de_rastreamento_de_entregas/ProjetoSistemaDeRastreamentoDeEntregasApplication.java
@@ -0,0 +1,13 @@
+package com.example.projeto_sistema_de_rastreamento_de_entregas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSistemaDeRastreamentoDeEntregasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSistemaDeRastreamentoDeEntregasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/projeto_sistema_de_rastreamento_de_entregas/ProjetoSistemaDeRastreamentoDeEntregasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projeto_sistema_de_rastreamento_de_entregas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSistemaDeRastreamentoDeEntregasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

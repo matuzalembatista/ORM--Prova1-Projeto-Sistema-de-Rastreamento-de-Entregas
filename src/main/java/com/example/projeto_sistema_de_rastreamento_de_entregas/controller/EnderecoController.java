b55c3f7158e172bd67b7bf3e1247b816f8b2609b
matuzalembatista <matuzalem_pereira@hotmail.com>
2024-09-02 22:03:06 -0300

    removendo valores e imports nao usado deixando o codigo mais limpo e evidando processamento

--- src/main/java/com/example/projeto_sistema_de_rastreamento_de_entregas/controller/EnderecoController.java
+++ src/main/java/com/example/projeto_sistema_de_rastreamento_de_entregas/controller/EnderecoController.java
@@ -2,14 +2,12 @@ package com.example.projeto_sistema_de_rastreamento_de_entregas.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.projeto_sistema_de_rastreamento_de_entregas.model.Endereco;
 import com.example.projeto_sistema_de_rastreamento_de_entregas.repository.EnderecoRepository;
-import com.example.projeto_sistema_de_rastreamento_de_entregas.service.EnderecoService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -17,8 +15,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RestController
 @RequestMapping("/enderecos")
 public class EnderecoController {
-
-    private EnderecoService enderecoService;
 
     private EnderecoRepository enderecoRepository;
 
